828ff735561bdb5f1c826b5ab55bee569ccf37ed
John Dewey <john@dewey.ws>
2012-09-15 23:15:06 -0700

    Init add of parted cookbook

---
+++ metadata.rb
@@ -0,0 +1,12 @@
+maintainer       "John Dewey"
+maintainer_email "john@dewey.ws"
+license          "Apache 2.0"
+description      "Installs/Configures parted"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.0.0"
+
+recipe           "parted", "Installs/Configures parted"
+
+%w{ centos debian ubuntu }.each do |os|
+  supports os
+end
